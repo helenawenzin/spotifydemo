e4ee63217c9fd0760398347a09a8a15e09a9c5a6
Helena Wenzin <helma254@hotmail.com>
2015-12-02 13:30:45 +0100

    added test for application

--- src/test/java/se/nackademin/SpotifydemoApplicationTests.java
+++ src/test/java/se/nackademin/SpotifydemoApplicationTests.java
@@ -2,18 +2,40 @@ package se.nackademin;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import se.nackademin.spotifydemo.SpotifydemoApplication;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SpotifydemoApplication.class)
-@WebAppConfiguration
+@WebIntegrationTest(randomPort = true)
+@DirtiesContext
 public class SpotifydemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+    @Value("${local.server.port}")
+	private int port;
+
+	@Test
+	public void testFreeMarkerTemplate() throws Exception {
+		ResponseEntity<String> entity = new TestRestTemplate()
+				.getForEntity("http://localhost:" + port, String.class);
+		assertEquals(HttpStatus.OK, entity.getStatusCode());
+		assertTrue("Wrong body:\n" + entity.getBody(),
+				entity.getBody().contains("Hejsan!"));
 	}
 
 }
